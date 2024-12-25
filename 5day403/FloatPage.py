5bf6a65a9f4b2eb8ab429e95b424da72e24307f2
DaminProNik <d20003n@gmail.com>
2024-12-25 21:38:52 +0000

    Add pseudo-binary number divisor sum function

---
+++ 5day403/FloatPage.py
@@ -0,0 +1,18 @@
+'''
+
+for i in range(1, 10):
+    print(i)
+'''
+
+
+def is_pseudo_binary(n):
+    for num in range(1, n):
+        if n % num == 0:
+            print(num)
+    divisors_sum = sum(i for i in range(1, n) if n % i == 0)
+    print('\nsum: ', divisors_sum)
+
+n = int(input().strip())
+print()
+
+is_pseudo_binary(n)
